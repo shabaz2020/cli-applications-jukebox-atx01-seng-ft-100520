9a921285830afc7cb917aa0d996a0c4323f40247
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-21 18:21:43 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,6 +21,7 @@ songs = [
 
 def help()
   puts "I accept the following commands:"
-  puts ":- help : displays this help message"
-  puts ""
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts 
   
