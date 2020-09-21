69c88e66e72a4167ee2abc4de9f624ea4a860b92
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-21 19:31:28 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,86 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+# THE HELP METHOD
+# This method should puts out the following:
+
+  # I accept the following commands:
+  # - help : displays this help message
+  # - list : displays a list of songs you can play
+  # - play : lets you choose a song to play
+  # - exit : exits this program
+  
+def help()
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+def list(my_songs)
+  my_songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end
+end
+
+
+def play(my_songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp()
+  
+  if (1..9).to_a.index(input.to_i) != nil
+    puts "Playing #{my_songs[input.to_i - 1]}"
+  elsif my_songs.index(input) != nil
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox()
+  puts "Goodbye"
+end
+
+def prompt()
+  puts "Please enter a command:"
+  gets.chomp()
+end
+
+
+def run(my_songs)
+  
+  input = ""
+  
+  while true do 
+    
+  input = prompt()
+    if input == "list"
+      list(my_songs)
+
+    elsif input == "play"
+      play(my_songs)
+      
+    elsif input == "help"
+      help()
+    elsif input == "exit"
+      exit_jukebox()
+     break
+    else
+      puts "Invalid command"
+  end
+end
+end
+
+
+
